fda276b37902c3477256c8fa6d5645cf579d510c
Tom Scott <tscott@weblinc.com>
2017-04-22 14:07:55 -0400

    Add index file docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,8 +2,11 @@ Apipie.configure do |config|
   config.app_name                = 'Wax Poetic Records API'
   config.api_base_url            = '/'
   config.doc_base_url            = '/docs'
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.api_controllers_matcher = Rails.root.join(
+    'app', 'controllers', '**', '*.rb'
+  ).to_s
   config.copyright = '&copy; 2017 Wax Poetic Records, LLC'
   config.markup = Apipie::Markup::Markdown.new
-  config.app_info = File.read Rails.root.join('docs', 'api.md')
+  index_path = Rails.root.join('docs', 'api.md')
+  config.app_info = File.read index_path
 end
